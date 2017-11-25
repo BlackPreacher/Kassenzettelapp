8ca692e531d67ee48ecb02ecdc61c01e64e2ae08
Hellhero <steffen.brossler@stud.h-da.de>
2017-11-25 23:04:25 +0100

    Initial commit

---
+++ app/src/main/java/com/development/black_preacher/uploadimage/AyncUploadTaskClass.java
@@ -0,0 +1,160 @@
+package com.development.black_preacher.uploadimage;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by bro on 25.11.2017.
+ */
+
+class AyncUploadTaskClass extends AsyncTask<Void, Void, String> {
+    Uri uri;
+    String server;
+    Context context;
+
+    ProgressDialog progressDialog;
+
+
+    public void setUri(Uri trans_uri){
+        uri = trans_uri;
+    }
+    public void setServer(String trans_Server){
+        server = trans_Server;
+    }
+
+    public void setContext(Context trans_context){
+        context = trans_context;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        StringBuilder stringBuilder = new StringBuilder();
+        String serverResponseMessage = null;
+        InputStream inputStream = null;
+        int maxBufferSize = 1 * 1024 * 1024;
+        int bytesAvailable = 0;
+        try {
+            inputStream = context.getContentResolver().openInputStream(uri);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        URL url = null;
+        HttpURLConnection conn = null;
+        try {
+            url = new URL(server+"/upload.php");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setDoInput(true); // Allow Inputs
+            conn.setDoOutput(true); // Allow Outputs
+            conn.setUseCaches(false); // Don't use a Cached Copy
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            conn.setRequestProperty("fileToUpload", "MyFile.jpg");
+
+            DataOutputStream dos = null;
+            dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"fileToUpload\";filename=" + "MyFile.jpg" + "" + lineEnd);
+            dos.writeBytes(lineEnd);
+
+
+            bytesAvailable = inputStream.available();
+
+            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            byte[] buffer = new byte[bufferSize];
+            int bytesRead = 0;
+            bytesRead = inputStream.read(buffer, 0, bufferSize);
+            while (bytesRead > 0) {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = inputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = inputStream.read(buffer, 0, bufferSize);
+            }
+
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            // Responses from the server (code and message)
+            int serverResponseCode = conn.getResponseCode();
+
+            serverResponseMessage = conn.getResponseMessage();
+
+            if (serverResponseCode == 200){
+                String result = readStream(conn.getInputStream());
+                System.out.println(result);
+            }
+            System.out.println(serverResponseCode);
+            System.out.println(serverResponseMessage);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return serverResponseMessage;
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressDialog = ProgressDialog.show(context, "Image is Uploading", "Please Wait", false, false);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        // Dismiss the progress dialog after done uploading.
+        progressDialog.dismiss();
+
+        // Printing uploading success message coming from server on android app.
+        Toast.makeText(context, "Upload Complete", Toast.LENGTH_LONG).show();
+
+        // Setting image as transparent after done uploading.
+        //imageView.setImageResource(android.R.color.transparent);
+
+
+    }
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
+    }
+}
---
+++ app/src/main/java/com/development/black_preacher/uploadimage/MainActivity.java
@@ -0,0 +1,160 @@
+package com.development.black_preacher.uploadimage;
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Bitmap bitmap;
+    Button btn_choose;
+    Button btn_send;
+    ImageView imageView;
+
+    Button btn_save;
+    EditText ed_Server;
+
+    SharedPreferences sharedPreferences;
+
+    String server;
+
+    Uri uri;
+
+    final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/";
+
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+            Manifest.permission.INTERNET
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_choose = findViewById(R.id.btn_choose);
+        btn_send = findViewById(R.id.btn_send);
+        imageView = findViewById(R.id.imageView);
+
+        ed_Server = findViewById(R.id.ed_server);
+        btn_save = findViewById(R.id.btn_save_server);
+
+        sharedPreferences = this.getSharedPreferences("com.development.black_preacher.uploadimage", Context.MODE_PRIVATE);
+        server = sharedPreferences.getString("link","");
+        //server = "http://pi3.hellhero.me/uploadTest";
+
+
+
+        btn_send.setEnabled(false);
+
+        ed_Server.setText(server);
+
+
+
+        int permission_write = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permission_read = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permission_camera = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int permission_internet = ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+
+        if (permission_write != PackageManager.PERMISSION_GRANTED ||
+                permission_camera != PackageManager.PERMISSION_GRANTED ||
+                permission_internet != PackageManager.PERMISSION_GRANTED ||
+                permission_read != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    200
+            );
+        }
+
+        // Wähle Bild aus Galerie
+        btn_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Image From Gallery"), 1);
+            }
+        });
+
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendImage();
+            }
+        });
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String serverLink = ed_Server.getText().toString();
+                sharedPreferences.edit().putString("link",serverLink).apply();
+                server = serverLink;
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println(requestCode);
+        System.out.println(resultCode);
+        System.out.println(data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uri = data.getData();
+
+            btn_send.setEnabled(true);
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+            //uri = data.getData();
+            System.out.println("CAMERA");
+            bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    public void sendImage() {
+
+        AyncUploadTaskClass uploadObjekt = new AyncUploadTaskClass();
+        uploadObjekt.setServer(server);
+        uploadObjekt.setUri(uri);
+        uploadObjekt.setContext(this);
+
+        uploadObjekt.execute();
+
+        imageView.setImageResource(android.R.color.transparent);
+        btn_send.setEnabled(false);
+
+
+    }
+}
